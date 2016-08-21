737fdd196cdf1196ba9dfa7b28c38c33c5237572
vino zhu <vino2014zly@gmail.com>
2016-08-21 23:30:53 +0800

    some function

--- src/main/java/TestPipeline.java
+++ src/main/java/TestPipeline.java
@@ -10,6 +10,7 @@ import java.util.Date;
 public class TestPipeline {
     public static void main(String[] args){
         unusePipeline();
+        
         usePipeline();
     }
     private static void usePipeline(){
